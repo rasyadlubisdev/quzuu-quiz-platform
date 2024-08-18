dc04dc3484b8401ebaec18cf7ba851260ce5bb9d
rasyadlubisdev <rasyadloebisa@gmail.com>
2024-08-18 23:32:01 +0700

    feat(quiz): navigation quiz

---
+++ src/app/event-details/[id]/start/[slug]/page.tsx
@@ -0,0 +1,26 @@
+import NavQuiz from "@/components/NavQuiz"
+
+const QuizPage = () => {
+    return (
+        <main className="quiz-page container bg-slate-100 text-slate-950 min-h-screen">
+            <section className="head-info py-8">
+                <h1 className="text-2xl font-bold">Try Out OSNK Informatika 2023</h1>
+                <h3 className="text-xl font-normal">#Bagian A : Pilihan Ganda dan Isian</h3>
+            </section>
+            <section className="pb-10 grid grid-cols-1 md:grid-cols-3 gap-y-8 md:gap-x-8">
+                <NavQuiz />
+                <div className="display-quiz col-span-2 bg-white p-9 rounded-3xl text-slate-800 shadow">
+                    <h1 className="text-2xl underline"># Question 1</h1>
+                    <ol className="list-decimal mt-5 ml-5">
+                        <li>Soal Ujian terdiri dari 2 jenis. Bagian A : Soal Pilihan Ganda dan Isian</li>
+                        <li>Dan Bagian B : Soal Membuat Program</li>
+                        <li>Lorem ipsum dolar si amet Lorem ipsum dolar si ametLorem ipsum dolar si amet</li>
+                        <li>Lorem ipsum dolar si amet Lorem ipsum dolar si ametLorem ipsum dolar si amet</li>
+                    </ol>
+                </div>
+            </section>
+        </main>
+    )
+}
+
+export default QuizPage
---
+++ src/components/NavQuiz.tsx
@@ -0,0 +1,45 @@
+"use client"
+
+import Link from "next/link"
+import { usePathname } from "next/navigation"
+import { Button } from "./ui/button"
+
+const NavQuiz = () => {
+    const pathname = usePathname()
+    // console.log(router)
+
+    return (
+        <div className="col-span-1 bg-white pt-4 pb-6 px-6 rounded-3xl text-slate-800 shadow">
+            <div className="timer px-6">
+                <h3 className="text-base font-bold text-secondary mb-1">Time Left</h3>
+                <div className="run-timer py-3 px-5 rounded-2xl border border-secondary">
+                    <h1 className="text-4xl font-bold text-secondary text-center">01 : 47 : 40</h1>
+                </div>
+            </div>
+            <div className="questions-number mt-5 flex flex-wrap justify-center">
+                <Link href="/" className="flex justify-center items-center rounded-md border-2 border-slate-400 text-slate-400 text-xl font-bold m-1.5" style={{ width: "54px", height: "47px" }}>01</Link>
+                <Link href="/" className="flex justify-center items-center rounded-md border-2 border-slate-400 text-slate-400 text-xl font-bold m-1.5" style={{ width: "54px", height: "47px" }}>02</Link>
+                <Link href="/" className="flex justify-center items-center rounded-md border-2 border-slate-400 text-slate-400 text-xl font-bold m-1.5" style={{ width: "54px", height: "47px" }}>03</Link>
+                <Link href="/" className="flex justify-center items-center rounded-md border-2 border-slate-400 text-slate-400 text-xl font-bold m-1.5" style={{ width: "54px", height: "47px" }}>04</Link>
+                <Link href="/" className="flex justify-center items-center rounded-md border-2 border-slate-400 text-slate-400 text-xl font-bold m-1.5" style={{ width: "54px", height: "47px" }}>05</Link>
+                <Link href="/" className="flex justify-center items-center rounded-md border-2 border-slate-400 text-slate-400 text-xl font-bold m-1.5" style={{ width: "54px", height: "47px" }}>06</Link>
+                <Link href="/" className="flex justify-center items-center rounded-md border-2 border-slate-400 text-slate-400 text-xl font-bold m-1.5" style={{ width: "54px", height: "47px" }}>07</Link>
+                <Link href="/" className="flex justify-center items-center rounded-md border-2 border-slate-400 text-slate-400 text-xl font-bold m-1.5" style={{ width: "54px", height: "47px" }}>08</Link>
+                <Link href="/" className="flex justify-center items-center rounded-md border-2 border-slate-400 text-slate-400 text-xl font-bold m-1.5" style={{ width: "54px", height: "47px" }}>09</Link>
+                <Link href="/" className="flex justify-center items-center rounded-md border-2 border-slate-400 text-slate-400 text-xl font-bold m-1.5" style={{ width: "54px", height: "47px" }}>10</Link>
+                <Link href="/" className="flex justify-center items-center rounded-md border-2 border-slate-400 text-slate-400 text-xl font-bold m-1.5" style={{ width: "54px", height: "47px" }}>11</Link>
+                <Link href="/" className="flex justify-center items-center rounded-md border-2 border-slate-400 text-slate-400 text-xl font-bold m-1.5" style={{ width: "54px", height: "47px" }}>12</Link>
+                <Link href="/" className="flex justify-center items-center rounded-md border-2 border-slate-400 text-slate-400 text-xl font-bold m-1.5" style={{ width: "54px", height: "47px" }}>13</Link>
+                <Link href="/" className="flex justify-center items-center rounded-md border-2 border-slate-400 text-slate-400 text-xl font-bold m-1.5" style={{ width: "54px", height: "47px" }}>14</Link>
+                <Link href="/" className="flex justify-center items-center rounded-md border-2 border-slate-400 text-slate-400 text-xl font-bold m-1.5" style={{ width: "54px", height: "47px" }}>15</Link>
+                <Link href="/" className="flex justify-center items-center rounded-md border-2 border-slate-400 text-slate-400 text-xl font-bold m-1.5" style={{ width: "54px", height: "47px" }}>16</Link>
+            </div>
+            <div className="buttons mt-4 flex flex-col">
+                <Button variant="outline" className="mb-4">Clarification</Button>
+                <Button>Submit Answers</Button>
+            </div>
+        </div>
+    )
+}
+
+export default NavQuiz
